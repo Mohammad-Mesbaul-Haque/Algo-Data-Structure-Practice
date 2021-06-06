f6c6e094a97ba7d39c0d7ff3cee361f120e8e1f6
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-06 17:29:29 +0600

    string reverse second solution added

--- Problems/reverse.js
+++ Problems/reverse.js
@@ -5,3 +5,15 @@ function strReverse(str) {
 }
 
 console.log(strReverse('I love you'));
+
+
+//second solution
+function strReverse2(str) {
+    let reversedStr = '';
+    for(let character of str){
+        reversedStr = character + reversedStr;
+    }
+    return reversedStr;
+}
+
+console.log(strReverse2('I love you'));
