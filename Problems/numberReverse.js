2c1c2bc8f710e1d1b55b30586c357e083753e225
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-06 17:59:19 +0600

    number reverse solved

--- Problems/numberReverse.js
+++ Problems/numberReverse.js
@@ -3,3 +3,11 @@
 * Then we reverse the converted string 
 * Lastly we convert the reversed string into number again
  */
+
+// solution 1
+function reverseNum(num) {
+    let reversed =  num.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(num);
+}
+
+console.log(reverseNum(-3000));
