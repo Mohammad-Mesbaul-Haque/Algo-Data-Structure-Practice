054ab2644f4ac601c9c3430f4ab8fa1a86ecb500
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-14 00:58:51 +0600

    inheritance problem added

---
+++ Problems/simpleInheritance.js
@@ -0,0 +1,15 @@
+//defining a supertype function
+function Animal() {} ;
+
+//add something in it's prototype chain
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log(`They are animals so various things are in their menu`);
+    }
+}
+
+// using Object.create method to make instance from Animal ;
+let dog = Object.create(Animal.prototype);
+
+console.log(dog.eat());
