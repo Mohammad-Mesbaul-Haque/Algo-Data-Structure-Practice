00fcc6681649a739d03a38415da7b81931590437
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-19 11:14:44 +0600

    Random string function created

--- Problems/createRandomString.js
+++ Problems/createRandomString.js
@@ -1,1 +1,23 @@
 // Random string creator function
+function createRandomString(stringLength) {
+    let strLength = stringLength;
+    strLength = typeof(stringLength) === 'number' && stringLength > 0 ? stringLength : false;
+
+    if(strLength){
+        let possibleCharacters = 'abcdefghijklmAQZWOd)(*&^nopqrstuvwxyz123456789!@#$%';
+        let output = '';
+        for(let i = 1; i <= strLength; i++){
+            let randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+
+            output += randomCharacter;
+        }
+        return output;
+    }
+    return false;
+}
+// call function
+console.log(createRandomString(10));
+console.log(createRandomString(5));
+console.log(createRandomString(8));
+console.log(createRandomString(12));
+console.log(createRandomString(15));
