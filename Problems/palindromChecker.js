6b42f67ccabdf1f90ca7eaaef43bf8c06ffa626d
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-07-19 19:44:21 +0600

    palindrome problem initiated

---
+++ Problems/palindromChecker.js
@@ -0,0 +1,8 @@
+/*
+ * Title: Palindrome Checker
+ * Description: Simple palindrome checker algorithm
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 19/07/2021
+ */
+
