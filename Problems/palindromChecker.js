4f8dbf3d6be1310acb157ef2b9ccb79901c78d39
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-07-19 20:07:55 +0600

    Palindrome checker trick 2 solved

--- Problems/palindromChecker.js
+++ Problems/palindromChecker.js
@@ -10,12 +10,29 @@
 function palindrome(string) {
     const reversedStr = string.split('').reverse().join('');
     if (string === reversedStr) {
-        return true;
+        return string + ' is palindrome';
     } else {
-        return false;
+        return string + ' is not a palindrome';
     }
 }
 
 
 console.log( palindrome('madam'));
 console.log( palindrome('hello'));
+
+
+// Trick 2
+function palindrome2(string) {
+    const strArray = string.split('');
+    for(let i = 0; i < strArray.length; i++){
+        if (strArray[i] === strArray[strArray.length - 1]) {
+            return string + ' is palindrome';
+        } else {
+            return string + ' is not a palindrome';
+        }
+    }
+}
+
+
+console.log( palindrome2('hello'));
+console.log( palindrome2('madam'));
