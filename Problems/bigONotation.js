153acef4a9161ef415518a275952f4835c960e43
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-09 23:17:19 +0600

    Big O Notation's shorthand writen

---
+++ Problems/bigONotation.js
@@ -0,0 +1,17 @@
+// What is Big O Notation?
+// # This term uses to explain any algorithm's time or space complexity's upper bound. Big O calculate the worst case of an algorithm. 
+// Contrary, Omega notation is opposite of Big O notation. Because It calculates the best case scenario. We can know from Ω notation how much performant it will be in it's best case scenario. 
+
+// Big O Shorthand:
+// 1. Arithmetic operations are constant.
+// 2. Variable assignments are constant.
+// 3. Accessing an array element using index number is constant.
+// 4. Accessing a property of object using key is constant.
+// 5. Any loop's complexity will be, inside the loop's operations complexity * how many times loop will be executed 
+
+// If we operate a loop on a single array or collection the big O notation might be - O(n) 
+// If we operate a loop on two array or collection individually , the big O notation might be - O(n +m)
+// If we operate a nested loop in on a single array or collection the big O notation might be - O(n²)
+// If we operate a nested loop on two array or collection individually, the big O notation might be - O(n * m)
+// Sorting algorithm's big O notation might be - O(n*log(n))
+// Sorted collection or array searching algorithm's big O notation might be - O(log(n))
